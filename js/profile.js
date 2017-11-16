6008f9f8df291b8e167096fa814f45138213c2ec
Dung Nguyen <dungnguyen@Dungs-MacBook-Pro.local>
2017-11-16 20:19:51 +0700

    update

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-		controlArrows: false,
-		verticalCentered: false,
-		sectionsColor : ['#000', '#fff', '#000', '#fff', '#000', '#fff'],
-	});
+	// $('#fullpage').fullpage({
+	// 	controlArrows: false,
+	// 	verticalCentered: false,
+	// 	sectionsColor : ['#000', '#fff', '#000', '#fff', '#000', '#fff'],
+	// });
 });
