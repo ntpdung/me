7226a8634e683da9f35b6ae3b7319ff47626c0e5
Dung Nguyen <dungnguyen@Dungs-MacBook-Pro.local>
2017-11-26 22:46:43 +0700

    update

--- js/profile.js
+++ js/profile.js
@@ -4,12 +4,19 @@ $(document).ready(function() {
 		verticalCentered: false
 	});
 	function init() {
-		// $('#page-wrap').hide();
+		$('#page-wrap').hide();
 		setTimeout(function () {
 			// debugger;
 			// $('#page-load').hide();
 			// $('#page-wrap').show();
 		}, 6000);
+		$('#scroll-down').click(function(){
+			$(this).hide();
+			$('#page-load').hide();
+			$('#page-wrap').show(500);
+		});
+
+		$(window).scrol();
 	}
 	init();
 });
